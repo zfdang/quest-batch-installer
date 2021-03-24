87c40155c56bb986fff0ca227cdf784a68f30382
Zhengfa Dang <zfdang@users.noreply.github.com>
2021-03-24 09:03:57 +0800

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,3 +1,21 @@
-# find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && ../AutoInstall.command" \;
+# verify device, and lunch AutoInstall in each app/game folder
+
+DIR="$(pwd)"
+ADB="$DIR/../adb"
+
+# check ADB status
+"$ADB" kill-server
+DEVICESTATUS="$("$ADB" devices | grep -v "List" | awk '{print $2}')"
+if [ "$DEVICESTATUS" != "device" ];then
+    echo "Your device is not connected or authorized. Please verify and try again!"
+    exit 1
+fi
+
+
+echo "Total Apps/Games:"
 find . -maxdepth 1 -type d \( ! -name . \) | wc -l
+echo "Now begin to install ..."
 find . -maxdepth 1 -type d \( ! -name . \) -print0 | sort -zn | xargs -0 -I '{}' bash -c "cd '{}' && ../AutoInstall.command" \;
+
+# kill adb at the end
+"$ADB" kill-server
